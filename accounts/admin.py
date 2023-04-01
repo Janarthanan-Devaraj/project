3992d45635bb670498fc4f8631d1d99433c48efe
jana-sanjay <djanarthanan614@gmail.com>
2023-04-01 14:00:06 +0530

    Branch 4 initial push

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, UserProfile, AcademicInfo, CompanyInfo
 
+admin.site.register(UserProfile)
+admin.site.register(AcademicInfo)
+admin.site.register(CompanyInfo)
 
 class CustomUserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserModelAdmin
     # that reference specific fields on auth.User.
-    list_display = ('id', 'email', 'student', 'alumni', 'is_superuser')
+    list_display = ('id','username', 'email', 'student', 'alumni','is_verified', 'is_superuser')
     list_filter = ('is_superuser',)
     fieldsets = (
-        ('User Credentials', {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('student', 'alumni')}),
+        ('User Credentials', {'fields': ('username','email', 'password')}),
+        ('Personal info', {'fields': ('student', 'alumni', 'is_verified')}),
         ('Permissions', {'fields': ('is_superuser',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserModelAdmin
@@ -20,14 +23,12 @@ class CustomUserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'student', 'alumni', 'password1', 'password2'),
+            'fields': ('username','email', 'student', 'alumni', 'password1', 'password2'),
         }),
     )
     search_fields = ('email',)
     ordering = ('email', 'id')
     filter_horizontal = ()
-
-
 
 # Register the CustomUser model with the admin site
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,23 +1,42 @@
-from django.urls import path
+from django.urls import path, include
 
-from .views import (SignUpView, LoginView, 
+from .views import (SignUpView, LoginView,  VerifyEmail,
                     UserProfileListCreateAPIView, 
                     AcademicInfoListCreateAPIView, 
                     CompanyInfoListCreateAPIView,
-                    UserRetrieveView)
+                    UserRetrieveView, ChangePasswordView,
+                    RegisterView, LoginAPIView, 
+                    SetNewPasswordAPIView, PasswordTokenCheckAPI, 
+                    RequestPasswordResetEmail)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+
+
+
+
 urlpatterns = [
+    path('signup/', RegisterView.as_view(),name='signup' ),
     path('register/', SignUpView.as_view(),name='register' ),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('signin/', LoginAPIView.as_view(), name='signin'),
     path('login/', LoginView.as_view(),name='login' ),
     path('profile/', UserRetrieveView.as_view(), name='profile'),
+    path('changepassword/', ChangePasswordView.as_view(), name='changepassword'),
+    path('password-reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('profile/create/', UserProfileListCreateAPIView.as_view(), name = 'profile-create'),
     path('academic-info/', AcademicInfoListCreateAPIView.as_view(), name='academic-info-list-create'),
     path('company-info/', CompanyInfoListCreateAPIView.as_view(), name='company-info-list-create'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete/', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
+    
 
 ]
